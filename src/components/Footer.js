221e48642275fdf6138592aa76989887ac4530f8
saulofilho <saulosilvaf@gmail.com>
2022-04-26 16:51:03 -0300

    update footer and types

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,24 +8,21 @@ const FooterWrapper = () => {
       <div className="footer-wrapper container">
         <div className="infos">
           <p>Av. ABC</p>
-          <p>Nº 666, Downtown</p>
-          <p>São Paulo, Brasil</p>
+          <p>Nº 123, Centro</p>
+          <p>Cuiabá, MT - Brasil</p>
         </div>
         <div className="infos">
-          <a href="https://instagram.com/">
+          <a href="https://instagram.com/bendito-cheiro-art">
             <Instagram color={"var(--green)"} />
           </a>
-          <a href="https://vimeo.com/">
-            <Film color={"var(--green)"} />
-          </a>
         </div>
         <div className="infos">
-          <p>hi@buenavistalab.com</p>
+          <p>ola@benditocheiroart.com</p>
         </div>
       </div>
       <div className="copyright">
         <p>
-          Copyright © 2022 buenavistalab softwares
+          Copyright © 2022 BENDITO CHEIRO ARTASANAL
           <br />
           All rights reserved.
         </p>
--- static/script.js
+++ static/script.js
@@ -6,36 +6,26 @@ var col = function (x, y, r, g, b) {
   $.fillRect(x, y, 1, 1);
 };
 var R = function (x, y, t) {
-  return Math.floor(192 + 64 * Math.cos((x * x - y * y) / 300 + t));
+  return Math.floor(150 + 64 * Math.cos((x * x - y * y) / 1000 + t));
 };
 
 var G = function (x, y, t) {
-  return Math.floor(
-    192 +
-      64 * Math.sin((x * x * Math.cos(t / 4) + y * y * Math.sin(t / 3)) / 300)
-  );
+  return Math.floor(150 + 64 * Math.sin((x * x * Math.cos(t / 4) + y * y * Math.sin(t / 3)) / 1000));
 };
 
 var B = function (x, y, t) {
-  return Math.floor(
-    192 +
-      64 *
-        Math.sin(
-          5 * Math.sin(t / 9) +
-            ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100
-        )
-  );
+  return Math.floor(150 + 64 * Math.sin(3 * Math.sin(t / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100));
 };
 
 var t = 0;
 
 var run = function () {
-  for (x = 0; x <= 35; x++) {
-    for (y = 0; y <= 35; y++) {
+  for (x = 0; x <= 55; x++) {
+    for (y = 0; y <= 55; y++) {
       col(x, y, R(x, y, t), G(x, y, t), B(x, y, t));
     }
   }
-  t = t + 0.02;
+  t = t + 0.05;
   window.requestAnimationFrame(run);
 };
 
